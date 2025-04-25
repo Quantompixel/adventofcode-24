7551fc6eae46b0d1a788bb1ca2f3f43c4ba2fc95
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2025-04-25 09:29:23 +0200

    day 3 part 2, finished

--- src/day_3/app.ts
+++ src/day_3/app.ts
@@ -1,24 +1,28 @@
 import * as fs from 'fs';
 
-// const path = './src/day_2/input/example.txt';
+// const path = './src/day_3/input/example.txt';
 const path = './src/day_3/input/input.txt';
 
 const file = fs.readFileSync(path, 'utf8');
 
-// const input = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))';
-const input = file;
-
-const mulRegex = /mul\(\d{1,3},\d{1,3}\)/g;
+const mulRegex = /(mul\(\d{1,3},\d{1,3}\))|(don't\(\))|(do\(\))/g;
 
 let result = 0;
 
-const multiplications = input.match(mulRegex);
+const instructions = file.match(mulRegex);
+console.log(instructions);
 
-if (multiplications) {
-    multiplications.forEach(multiplication => {
-        console.log(multiplication)
+if (instructions) {
+    let disabled = false;
+
+    instructions.forEach(instruction => {
+        console.log(instruction)
+        if (instruction == `don't()`) disabled = true;
+        if (instruction == 'do()') disabled = false;
+        if (disabled) return;
+
         const numberRegex = /\d+/g;
-        const matches = multiplication.match(numberRegex);
+        const matches = instruction.match(numberRegex);
         console.log(matches)
 
         if (!matches) return;
