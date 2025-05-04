0ea4fc4ae033c8ac817c6d40a608391a2c73a4a6
Quantompixel <schodllukas8@gmail.com>
2025-05-04 12:32:01 +0200

    day 5 part 2, finished

--- src/day_5/app.ts
+++ src/day_5/app.ts
@@ -1,8 +1,8 @@
 import * as fs from 'fs';
 import * as os from 'os';
 
-const path = './src/day_5/input/example.txt';
-// const path = './src/day_5/input/input.txt';
+// const path = './src/day_5/input/example.txt';
+const path = './src/day_5/input/input.txt';
 
 const file = fs.readFileSync(path, 'utf8');
 const lines = file.trim().split(os.EOL);
@@ -34,7 +34,7 @@ parseRules();
 parsePageNumberGroups();
 
 for (const pageNumbers of pageNumberGroups) {
-    const checkPageNumbers = (pageNumbers: number[]) => {
+    const checkPageNumbers = (pageNumbers: number[], iteration = 0) => {
         let isIncorrect = false;
 
         for (const [pageNumber1, pageNumber2] of rules) {
@@ -47,20 +47,21 @@ for (const pageNumbers of pageNumberGroups) {
 
             if (index1 > index2) {
                 isIncorrect = true;
-                // console.log(pageNumber1, pageNumber2);
-                // console.log(pageNumbers, 'incorrect');
+
+                // switches numbers into the correct order
                 const part1 = pageNumbers.slice(0, index2);
                 const part2 = pageNumbers.slice(index2, pageNumbers.length);
+
                 part2.splice(part2.indexOf(pageNumber1), 1);
-                // console.log(part1);
-                // console.log(part2);
                 pageNumbers = part1.concat(pageNumber1).concat(part2);
-                // console.log(pageNumbers, 'corrected');
             }
         }
-        if (isIncorrect) checkPageNumbers(pageNumbers);
+        if (isIncorrect) checkPageNumbers(pageNumbers, iteration + 1);
         else {
-            console.log(pageNumbers);
+            console.log(pageNumbers, iteration);
+            if (iteration > 0) {
+                res += pageNumbers[Math.floor(pageNumbers.length / 2)];
+            }
             return;
         }
     }
@@ -68,4 +69,4 @@ for (const pageNumbers of pageNumberGroups) {
     checkPageNumbers(pageNumbers);
 }
 
-// console.log(res);
+console.log(res);
