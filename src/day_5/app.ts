35c0bb35d02ac25349cf99fb7ca6a43775975885
Quantompixel <schodllukas8@gmail.com>
2025-05-04 11:19:31 +0200

    day 5 part 1, formatted data

--- src/day_5/app.ts
+++ src/day_5/app.ts
@@ -1,9 +1,33 @@
 import * as fs from 'fs';
+import * as os from 'os';
 
 const path = './src/day_5/input/example.txt';
 // const path = './src/day_4/input/input.txt';
 
 const file = fs.readFileSync(path, 'utf8');
-const lines = file.trim().split('\r\n');
+const lines = file.trim().split(os.EOL);
+const rules: Array<[number, number]> = [];
+const pageNumbers: Array<Array<number>> = [];
 
-console.log(lines)
+function parseRules() {
+    const data = lines.slice(0, lines.indexOf(''));
+
+    data.forEach(line => {
+        const seperatedLine = line.split('|');
+        const pageNumber1 = parseInt(seperatedLine[0]);
+        const pageNumber2 = parseInt(seperatedLine[1])
+
+        rules.push([pageNumber1, pageNumber2]);
+    })
+}
+
+function parsePageNumbers() {
+    const data = lines.slice(lines.indexOf('') + 1, lines.length);
+    data.forEach(value => {
+        const numArr = value.split(',').map(num => parseInt(num));
+        pageNumbers.push(numArr);
+    })
+}
+
+parseRules();
+parsePageNumbers();
