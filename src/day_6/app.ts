95ea5f32f003f972701d5e1ce32528ac217cf1ab
Quantompixel <39237785+Quantompixel@users.noreply.github.com>
2025-05-06 20:01:28 +0200

    day 5 part 2, started

--- src/day_6/app.ts
+++ src/day_6/app.ts
@@ -1,8 +1,8 @@
 import * as fs from 'fs';
 import * as os from 'os';
 
-// const path = './src/day_6/input/example.txt';
-const path = './src/day_6/input/input.txt';
+const path = './src/day_6/input/example.txt';
+// const path = './src/day_6/input/input.txt';
 
 const file = fs.readFileSync(path, 'utf8');
 const lines = file.trim().split(os.EOL);
@@ -21,7 +21,13 @@ lines.forEach((line, row) => {
     map.push(seperatedLine);
 });
 
-function traverseMap([yPos, xPos]: [number, number], [yDir, xDir]: [number, number], map: string[][], step = 0) {
+function traverseMap(
+    [yPos, xPos]: [number, number],
+    [yDir, xDir]: [number, number],
+    map: string[][],
+    step = 0,
+    previousPositions: Array<{position: [number, number], direction: [number, number]}> = []
+) {
     const rotateClockwise = (currentDirection: [number, number]) => {
         const directions: Array<[number, number]> = [[-1, 0], [0, 1], [1, 0], [0, -1]];
         const index = directions.findIndex(value => value[0] === currentDirection[0] && value[1] === currentDirection[1]);
@@ -33,6 +39,18 @@ function traverseMap([yPos, xPos]: [number, number], [yDir, xDir]: [number, numb
         return directions[index + 1];
     }
 
+    const isPositionInArray = (position: {position: [number, number], direction: [number, number]}): boolean => {
+        for (let previousPosition of previousPositions) {
+            if (previousPosition.position[0] === position.position[0] &&
+                previousPosition.position[1] === position.position[1] &&
+                previousPosition.direction[0] === position.direction[0] &&
+                previousPosition.direction[1] === position.direction[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     if (map[yPos][xPos] !== '%') {
         map[yPos][xPos] = '%';
         res++;
@@ -41,7 +59,6 @@ function traverseMap([yPos, xPos]: [number, number], [yDir, xDir]: [number, numb
     // map.forEach(row => console.log(row.join('')));
 
     let nextPosition = [yPos + yDir, xPos + xDir];
-    console.log(nextPosition);
 
     if (nextPosition[0] < 0 || nextPosition[0] > map.length - 1) return;
     if (nextPosition[1] < 0 || nextPosition[1] > map[nextPosition[0]].length - 1) return;
@@ -54,7 +71,14 @@ function traverseMap([yPos, xPos]: [number, number], [yDir, xDir]: [number, numb
     if (nextPosition[0] < 0 || nextPosition[0] > map.length - 1) return;
     if (nextPosition[1] < 0 || nextPosition[1] > map[nextPosition[0]].length - 1) return;
 
-    traverseMap([nextPosition[0], nextPosition[1]], [yDir, xDir], map, step + 1);
+    if (isPositionInArray({position: [yPos, xPos], direction: [yDir, xDir]})) {
+        previousPositions.push({position: [yPos, xPos], direction: [yDir, xDir]});
+        console.log({position: [yPos, xPos], direction: [yDir, xDir]});
+        console.log(previousPositions);
+        return;
+    }
+
+    traverseMap([nextPosition[0], nextPosition[1]], [yDir, xDir], map, step + 1, previousPositions);
     // console.log(yDir, xDir);
     // [yDir, xDir] = rotateClockwise([yDir, xDir]);
     // console.log(yDir, xDir);
