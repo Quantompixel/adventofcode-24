5b3d58b5dd0ed3fa8fba234c978f4b40b0a9c45f
Quantompixel <schodllukas8@gmail.com>
2025-05-18 13:27:13 +0200

    day 8 part 1, finished

--- src/day_8/app.ts
+++ src/day_8/app.ts
@@ -1,22 +1,29 @@
 import * as fs from 'fs';
 import * as os from 'os';
 
-const path = './src/day_8/input/example.txt';
-// const path = './src/day_8/input/input.txt';
+// const path = './src/day_8/input/example.txt';
+const path = './src/day_8/input/input.txt';
 
 const file = fs.readFileSync(path, 'utf8');
 const lines = file.trim().split(os.EOL);
 
-const allAntennas: {[key: string]: Array<{id: number, y: number, x: number}>} = {};
+const map = lines.map(line => line.split(''));
+const allAntennas: { [key: string]: Array<{ id: number, y: number, x: number }> } = {};
+const antinodePositions: Array<{ y: number, x: number }> = [];
 
+function isPositionInPositionArray(position: { y: number, x: number }, positionArray: Array<{ y: number, x: number }>) {
+    for (const positionOfArray of positionArray) {
+        if (positionOfArray.y === position.y && positionOfArray.x === position.x) return true;
+    }
+}
 
-lines.forEach((line, y) => {
-    line.split('').forEach((character, x) => {
-        if(character === '.') return;
-        if(!allAntennas[character]) allAntennas[character] = [];
+map.forEach((row, y) => {
+    row.forEach((character, x) => {
+        if (character === '.') return;
+        if (!allAntennas[character]) allAntennas[character] = [];
 
         allAntennas[character].push({
-            id: y * line.length + x,
+            id: y * map.length + x,
             y: y,
             x: x
         })
@@ -24,4 +31,31 @@ lines.forEach((line, y) => {
 });
 
 
-console.log(allAntennas);
+for (const antennaGroupKey in allAntennas) {
+    const antennaGroup = allAntennas[antennaGroupKey];
+    for (const antenna of antennaGroup) {
+        for (const neighborAntenna of antennaGroup) {
+            if (neighborAntenna.id === antenna.id) continue;
+
+            const yDiff = neighborAntenna.y - antenna.y;
+            const xDiff = neighborAntenna.x - antenna.x;
+
+            const antinodeY = antenna.y + 2 * yDiff;
+            const antinodeX = antenna.x + 2 * xDiff;
+
+            if (antinodeY < 0 || antinodeY > map.length - 1) continue;
+            if (antinodeX < 0 || antinodeX > map[antinodeY].length - 1) continue;
+
+            if (map[antinodeY][antinodeX] === '.') {
+                map[antinodeY][antinodeX] = '#';
+            }
+
+            if (!isPositionInPositionArray({ y: antinodeY, x: antinodeX }, antinodePositions)) {
+                antinodePositions.push({ y: antinodeY, x: antinodeX });
+            }
+        }
+    }
+}
+
+map.forEach(row => console.log(row.join('')));
+console.log(antinodePositions.length);
